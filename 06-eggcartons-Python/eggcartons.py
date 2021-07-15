b2e9c1fc1fb008386ff7095acee39be7511e0c27
sowmyareddy98 <sowmyareddy077@msitprogram.net>
2021-07-15 09:03:54 +0530

    egg cartons

--- 06-eggcartons-Python/eggcartons.py
+++ 06-eggcartons-Python/eggcartons.py
@@ -6,4 +6,11 @@
 
 def fun_eggcartons(eggs):
 	# your code goes here
-	return 1
+	if (eggs<=0):
+	 	return 0
+	elif(eggs>0 and eggs<=12):
+		return 1
+	elif(eggs>12 and eggs<=24):
+		return 2
+	elif(eggs>24 and eggs<=36):
+		return 3
