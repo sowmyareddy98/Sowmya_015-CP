6201f6b25e36a5926b50ac35e39c5c7082e0802a
sowmyareddy98 <sowmyareddy077@msitprogram.net>
2021-07-16 20:36:59 +0530

    righttriangle

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -8,4 +8,11 @@
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
 	#pass
-	
+    A = (int(pow((x2 - x1), 2)) + int(pow((y2 - y1), 2)))
+    B = (int(pow((x3 - x2), 2)) + int(pow((y3 - y2), 2)))
+    C = (int(pow((x3 - x1), 2)) + int(pow((y3 - y1), 2)))
+    
+    if ((A > 0 and B > 0 and C > 0) and (A == (B + C) or B == (A + C) or C == (A + B))):
+        return True
+    else:
+        return False
