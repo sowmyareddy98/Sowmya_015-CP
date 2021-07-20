9d85c3582801945ef4d5c643c801ac1209afdb60
sowmyareddy98 <sowmyareddy077@msitprogram.net>
2021-07-20 20:16:09 +0530

    incomplete

--- makeMagicSquare.py
+++ makeMagicSquare.py
@@ -35,4 +35,27 @@
 
 def makeMagicSquare(n):
     # Your code goes here...
-    pass
+    #pass
+  mat= len(n)
+  # sumd1 and sumd2 are the sum of the two diagonals
+  sumd1=0
+  sumd2=0
+  for i in range(mat):
+    # (i, i) is the diagonal from top-left -> bottom-right
+    # (i, n - i - 1) is the diagonal from top-right -> bottom-left
+        sumd1+=mat[i][i]
+        sumd2+=mat[i][n-i-1]
+    # if the two diagonal sums are unequal then it is not a magic square
+  if not(sumd1==sumd2):
+    return False
+  for i in range(mat):
+    #sumr is rowsum and sumc is colsum
+    sumr=0
+    sumc=0
+    for j in range(mat):
+      sumr+=mat[i][j]
+      sumc+=mat[j][i]
+    if not(sumr==sumc==sumd1):
+      return False
+    #if all the conditions are satisfied then it is a magic square
+  return True
