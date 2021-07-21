872d4352ec0d163cb30f3f02ce9b31b5a1781e7b
sowmyareddy98 <sowmyareddy077@msitprogram.net>
2021-07-21 22:26:23 +0530

    ishappynumber

--- 11-ishappynumber-Python/ishappynumber.py
+++ 11-ishappynumber-Python/ishappynumber.py
@@ -17,20 +17,42 @@
 # assert(ishappynumber(404) == True)
 # assert(ishappynumber(405) == False)
 
+def numSquareSum(n):
+    squareSum = 0;
+    while(n):
+        squareSum += (n % 10) * (n % 10);
+        n = int(n / 10);
+    return squareSum;
+ 
+# method return true if
+# n is Happy number
 def ishappynumber(n):
-		# your code goes here
-	#pass
-	temp=n
-	sum=0
-	while(temp>0):
-		r=n%10
-		sum=sum+r*r
-		temp=temp//10
-	temp=sum
-
-while(sum!= 1 and sum!= 4): 
-	if(sum==1):
-     	return True
-	else:
-		return False
-	 
+ 
+    # initialize slow
+    # and fast by n
+    slow = n;
+    fast = n;
+    while(True):
+         
+        # move slow number
+        # by one iteration
+        slow = numSquareSum(slow);
+ 
+        # move fast number
+        # by two iteration
+        fast = numSquareSum(numSquareSum(fast));
+        if(slow != fast):
+            continue;
+        else:
+            break;
+ 
+    # if both number meet at 1,
+    # then return true
+    return (slow == 1);
+ 
+# Driver Code
+n = 13;
+if (ishappynumber(n)):
+    print(n , "is a Happy number");
+else:
+    print(n , "is not a Happy number");
