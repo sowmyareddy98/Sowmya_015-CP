632f63fcdae964fcc594a7b99da7fe51255831a1
sowmyareddy98 <sowmyareddy077@msitprogram.net>
2021-07-22 17:24:42 +0530

    linkedlist

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -14,7 +14,7 @@ class Element(object):
         self.next = None
         
 class LinkedList(object):
-    def __init__(self, head=None):P
+    def __init__(self, head=None):
         self.head = head
         
     def append(self, new_element):
