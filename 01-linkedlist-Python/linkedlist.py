1f526d1fe353e7bfa3be4d4f30f37849bf1e4504
sowmyareddy98 <sowmyareddy077@msitprogram.net>
2021-07-23 13:47:45 +0530

    linkedlist

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -9,35 +9,74 @@ Then, use "Test Run" and "Submit" to run the test cases
 at the bottom."""
 
 class Element(object):
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-        
+	def __init__(self, value):
+		self.value = value
+		self.next = None
+		
 class LinkedList(object):
-    def __init__(self, head=None):P
-        self.head = head
-        
-    def append(self, new_element):
-        # Your code goes here
-        pass
-            
-    def get_position(self, position):
-        """Get an element from a particular position.
-        Assume the first position is "1".
-        Return "None" if position is not in the list."""
-        # Your code goes here
-        pass
-    
-    def insert(self, new_element, position):
-        """Insert a new node at the given position.
-        Assume the first position is "1".
-        Inserting at position 3 means between
-        the 2nd and 3rd elements."""
-        # Your code goes here
-        pass
-    
-    
-    def delete(self, value):
-        """Delete the first node with a given value."""
-        # Your code goes here
-        pass
+	def __init__(self, head=None):
+		self.head = head
+		
+	def append(self, new_element):
+		# Your code goes here
+		#pass
+		current = self.head
+		if self.head:
+			while current.next!=None:
+				current = current.next
+			current.next = new_element
+		else:
+			self.head = new_element
+			
+	def get_position(self, position):
+		"""Get an element from a particular position.
+		Assume the first position is "1".
+		Return "None" if position is not in the list."""
+		# Your code goes here
+		#pass
+		counter = 1
+		current = self.head
+		if position < 1:
+			return None
+		while current and counter <= position:
+			if counter == position:
+				return current
+			current = current.next
+			counter += 1
+		return None
+		
+
+	def insert(self, new_element, position):
+		"""Insert a new node at the given position.
+		Assume the first position is "1".
+		Inserting at position 3 means between
+		the 2nd and 3rd elements."""
+		# Your code goes here
+		#pass
+		if position == 1:
+			new_element.next = self.head
+			self.head = new_element
+		else:
+			prev = self.get_position(position - 1)
+			next_element = prev.next
+			#assign new connections
+			prev.next = new_element
+			new_element.next = next_element
+	
+		 
+	
+	def delete(self, value):
+		"""Delete the first node with a given value."""
+		# Your code goes here
+		#pass
+		current = self.head
+		previous = None
+		while current.value != value and current.next:
+			previous = current
+			current = current.next
+		if current.value == value:
+			if previous:
+				previous.next = current.next
+			else:
+				self.head = current.next
+			
