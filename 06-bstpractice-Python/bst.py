deb5e01baf15f40cb32f9db5c119112ee344270b
sowmyareddy98 <sowmyareddy077@msitprogram.net>
2021-07-27 16:44:44 +0530

    commit

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -4,7 +4,6 @@ class Node(object):
         self.left = None
         self.right = None
 
-
 class BinaryTree(object):
     def _init_(self, root):
         self.root = Node(root)
@@ -14,20 +13,20 @@ class BinaryTree(object):
         Return True if the find_val is in the tree and False otherwise.
         """
         # Your code goes here
-        return self.preorder_search(self.root, find_val)
+        return self.preorder_search(self.root,find_val)
 
     def print_tree(self):
         """
         Print out all tree nodes as they are visited in a pre-order traversal."""
         # Your code goes here
-        return self.preorder_print(self.root, "")[:-1]
+        return self.preorder_print(self.root,"")[:-1]
 
     def preorder_search(self, start, find_val):
         """
         Helper method - use this to create a recursive search solution.
         """
         # Your code goes here
-        return start.value == find_val or self.preorder_search(start.left, find_val) or self.preorder_search(start.right, find_val) if start else False
+        return start.value == find_val or self.preorder_search(start.right,find_val) if start else False
 
     def preorder_print(self, start, traversal):
         """
@@ -36,6 +35,6 @@ class BinaryTree(object):
         # Your code goes here
         if start:
             traversal += (str(start.value) + "-")
-            traversal = self.preorder_print(start.left, traversal)
-            traversal = self.preorder_print(start.right, traversal)
-        return traversal
+            traversal = self.preorder_print(start.left,traversal)
+            traversal = self.preorder_print(start.right,traversal)
+            return traversal
