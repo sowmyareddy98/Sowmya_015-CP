95a5da1c12a089f766a3a736fe28d28289f4210f
sowmyareddy98 <sowmyareddy077@msitprogram.net>
2021-07-27 18:01:58 +0530

    cmmt

--- 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
+++ 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
@@ -3,21 +3,25 @@
 # number itself. For example, 5^2 = 25, 6^2 = 36, 76^2 = 5776, and 890625^2 = 793212890625, so 5, 6, 
 # 76 and 890625 are all automorphic numbers.
 
-def range(n):
-    sq=int(n*n)
-    while n>0:
-        if n%10 != sq%10:
-            return False
-        n=n//10
-        sq=sq//10
-    return True
+def Automorphic(n):
+    z=n
+    a=n**2
+    total=0
+    while (z>0):
+        z=z//10
+        total=total+1
+        if(z==0):
+            break
+    b=a%(10**total)
+    if(b==n):
+        return b
+    return False
 
 def nthautomorphicnumbers(n):
-    # Your code goes here
-    f=1
-    g=-1
-    while(f<=abs(n)):
-        g=g+1
-        if(range(g)):
-            f=f+1
-    return g
+    total=2
+    i=1
+    while (total<=n):
+        if(Automorphic(i)):
+          total=total+1
+        i=i+1
+    return i-1
