c5d6490461d92b3319762290aeeb45a8872ae1df
sowmyareddy98 <sowmyareddy077@msitprogram.net>
2021-07-27 17:09:39 +0530

    commit

--- 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
+++ 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
@@ -3,6 +3,3 @@
 # number itself. For example, 5^2 = 25, 6^2 = 36, 76^2 = 5776, and 890625^2 = 793212890625, so 5, 6, 
 # 76 and 890625 are all automorphic numbers.
 
-def nthautomorphicnumbers(n):
-	# Your code goes here
-	pass
